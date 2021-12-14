f7ca497aaecc93d723a1fe432412e935aa4bf583
peteto1213 <peteto1213@gmail.com>
2021-12-14 23:55:04 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) =>{
+
+    const toggleButton = document.getElementsByClassName("toggle-button")[0];
+    const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+    toggleButton.addEventListener('click', () =>{
+        navbarLinks.classList.toggle('active');
+    })
+
+
+
+})
