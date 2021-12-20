e32f19684c7f962240ea6a9fa41cfeb176749a3d
peteto1213 <peteto1213@gmail.com>
2021-12-20 00:28:17 +0000

    update project and blog section

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     const toggleButton = document.getElementsByClassName("toggle-button")[0];
     const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
+    // show navbar column
     toggleButton.addEventListener('click', () =>{
         navbarLinks.classList.toggle('active');
     })
@@ -10,7 +11,23 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     // navbar will also close if any of the list item is clicked
     navbarLinks.addEventListener('click', () =>{
     navbarLinks.classList.toggle('active');
-})
+    }) 
+
+    // show projects according to chosen category filter
+    $('.portfolio .button-container .btn').click(function(){
+
+        let filter = $(this).attr('data-filter');
+
+        if(filter == 'all'){
+            $('.portfolio .image-container .box').show('400')
+        }
+        else{
+            // '.' + filter, if filter = design --> '.design'
+            $('.portfolio .image-container .box').not('.'+filter).hide('200');
+            $('.portfolio .image-container .box').filter('.'+filter).show('400');
+        }
+
+    });
 
 
 
