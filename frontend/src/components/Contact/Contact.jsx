1c3349a46528db43512cbfa2ac91078a9fb3b5f5
Pete To <peteto1213@gmail.com>
2022-10-14 20:49:28 +0800

    contact section layout design for contact card

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaCommentDots } from 'react-icons/fa'
+import { FaCommentDots, FaEnvelope, FaPhoneSquareAlt } from 'react-icons/fa'
 
 function Contact() {
   return (
@@ -9,7 +9,35 @@ function Contact() {
             <h1>Contact <span>Me</span></h1>
         </div>
 
-        
+        <div className="contact-container">
+          <div className="contact-card">
+            <FaEnvelope className='icon' />
+            <h3>peteto1213@gmail.com</h3>
+          </div>
+
+          <div className="contact-card">
+            <FaPhoneSquareAlt className='icon' />
+            <h3>+852 64875640</h3>
+          </div>
+        </div>
+
+        <div className="form-container">
+          <form>
+            <input 
+              type="text" 
+              placeholder='Your Name'
+            />
+            <input 
+              type="text" 
+              placeholder='Your Email'
+            />
+            <input 
+              type="text" 
+              placeholder='Your Message'
+            />
+            <button>Send Message</button>
+          </form>
+        </div>
 
     </section>
   )
