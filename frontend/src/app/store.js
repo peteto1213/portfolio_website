77d3cb223db5acb177b4b90e9325e8804e2f4977
Pete To <peteto1213@gmail.com>
2023-02-05 12:26:36 +0800

    added blogSlice to redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import BlogReducer from '../features/blog/blogSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    
+    blog: BlogReducer,
   },
 });
